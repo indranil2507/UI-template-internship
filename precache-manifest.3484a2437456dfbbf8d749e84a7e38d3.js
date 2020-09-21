022528bf49feb9ed1f366b71d3cbab62c97f5f47
indranil2507 <iindranilsaha1@gmail.com>
2020-09-21 16:32:27 +0530

    Updates

---
+++ precache-manifest.3484a2437456dfbbf8d749e84a7e38d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f23942e9778a8a4aa367fba2dafc3c",
+    "url": "/UI-template-internship/index.html"
+  },
+  {
+    "revision": "6000f604b976afa97a1d",
+    "url": "/UI-template-internship/static/css/2.fb53b6aa.chunk.css"
+  },
+  {
+    "revision": "83ebdb38a2db0d293ec0",
+    "url": "/UI-template-internship/static/css/main.6ebfabbd.chunk.css"
+  },
+  {
+    "revision": "6000f604b976afa97a1d",
+    "url": "/UI-template-internship/static/js/2.405d4dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/UI-template-internship/static/js/2.405d4dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ebdb38a2db0d293ec0",
+    "url": "/UI-template-internship/static/js/main.44f5358a.chunk.js"
+  },
+  {
+    "revision": "b9e3ce29c196b5f8ff83",
+    "url": "/UI-template-internship/static/js/runtime-main.fb2243c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/UI-template-internship/static/media/logo.5d5d9eef.svg"
+  }
+]);
